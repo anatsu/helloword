9834068ad0e44f6eb374ac87683d3aa5d4cb8fbe
anatsu <alvaro@localhost.localdomain>
2017-12-17 02:18:04 -0600

    me faltan cambios sobre A

--- A.java
+++ A.java
@@ -1,2 +1,6 @@
 public class A{
+
+	public void nada()
+	{}
+
 }
--- Algo.java
+++ Algo.java
@@ -1,10 +1,12 @@
 
 public class Algo{
 
-	A a = new A();
+	static A a = new A();
 
 	public static void main( String[] args)
 	{
 		System.out.println("he fallado");
+		a = null;
+		a.nada();
 	}
 }
